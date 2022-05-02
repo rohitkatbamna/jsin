5fb2b7615b44e37f0ff2c3142ab90c5f5631e169
rohit katbamna <rohitkatbamna@protonmail.com>
2022-05-02 11:50:08 +0530

    10:16 AM Q34 GENERATORS ADDED

--- src/test.js
+++ src/test.js
@@ -1,10 +1,11 @@
-const obj = {
-	start: 10,
-	end: 15,
-	*[Symbol.iterator]() {
-		for (let cnt = this.start; cnt <= this.end; cnt++) {
-			yield cnt;
-		}
-	},
-};
-console.log([...obj]);
+function* gen() {
+	try {
+		yield "One";
+		yield "Two";
+	} catch (err) {
+		console.log("Error " + err);
+	}
+}
+const gobj = gen();
+console.log(gobj.next());
+console.log(gobj.throw());
